d591deb4662a884c84296d39a217e5549bd0311b
Anthony Martin <anthonymartin20@gmail.com>
2023-03-06 15:31:13 +0100

    Install Linters

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Router from './Router';
 function App() {
   return (
     <div className="App">
-     <Router />
+      <Router />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import App from './App';
 import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Provider store={store} >
+    <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>,
