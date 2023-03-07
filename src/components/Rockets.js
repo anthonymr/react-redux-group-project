46d52c3a9b13cfe95cfc93dcc0e3776ce61caadd
Anthony Martin <anthonymartin20@gmail.com>
2023-03-07 10:20:53 +0100

    Styling Rockets component

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,16 +1,21 @@
 import { useSelector } from 'react-redux';
 import Rocket from './Rocket';
+import styles from '../styles/Rockets.module.css';
 
 function Rockets() {
   const { rockets } = useSelector((state) => state.rockets);
 
   return (
-    <section>
-      <ul>
+    <section className={styles.rocketsContainer}>
+      <ul className={styles.rocketList}>
         {
           rockets.map((rocket) => (
             <li key={rocket.id}>
-              <Rocket flickrImages={rocket.flickr_images} name={rocket.name} description={rocket.description} />
+              <Rocket
+                flickrImages={rocket.flickr_images}
+                name={rocket.name}
+                description={rocket.description}
+              />
             </li>
           ))
         }
