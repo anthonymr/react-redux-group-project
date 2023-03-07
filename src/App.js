76fda3fb2e92fb8ca9c80860fb82945c0591072d
Anthony Martin <anthonymartin20@gmail.com>
2023-03-07 09:32:59 +0100

    Dispatching getRockets first time Rockets loads

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getRockets } from './redux/rockets/rocketsSlice';
 import Router from './Router';
 import Navbar from './components/Navbar';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Navbar />
