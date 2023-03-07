1be72d6e952815c92a79a601f8d334e80d860682
Anthony Martin <anthonymartin20@gmail.com>
2023-03-07 10:20:44 +0100

    Styling Rocket component

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,12 +1,13 @@
 import PropTypes from 'prop-types';
+import styles from '../styles/Rocket.module.css';
 
 function Rocket({ flickrImages, name, description }) {
   return (
-    <article>
-      <div>
+    <article className={styles.rocketContainer}>
+      <div className={styles.rocketPicture}>
         <img src={flickrImages[0]} alt={name} />
       </div>
-      <div>
+      <div className={styles.rocketContent}>
         <h2>{name}</h2>
         <p>{description}</p>
         <button type="button">Reserve Rocket</button>
@@ -16,7 +17,7 @@ function Rocket({ flickrImages, name, description }) {
 }
 
 Rocket.propTypes = {
-  flickrImages: PropTypes.arrayOf([]).isRequired,
+  flickrImages: PropTypes.arrayOf(Array).isRequired,
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
 };
