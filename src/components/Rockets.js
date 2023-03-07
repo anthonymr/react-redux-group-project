dc774f283040469d84230a23673aaa399bd82152
Anthony Martin <anthonymartin20@gmail.com>
2023-03-07 10:00:30 +0100

    Display all rockets

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,20 @@
+import { useSelector } from 'react-redux';
+import Rocket from './Rocket';
+
 function Rockets() {
+  const { rockets } = useSelector((state) => state.rockets);
+
   return (
     <section>
-      Rockets
+      <ul>
+        {
+          rockets.map((rocket) => (
+            <li key={rocket.id}>
+              <Rocket flickrImages={rocket.flickr_images} name={rocket.name} description={rocket.description} />
+            </li>
+          ))
+        }
+      </ul>
     </section>
   );
 }
