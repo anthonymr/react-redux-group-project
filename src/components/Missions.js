d62c9596437542cfb3a1e5f424547926e94fa90f
Mohammad Rafi Amin <mrkamin2@gmail.com>
2023-03-08 16:00:03 +0500

    Adding Redux Hook

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,7 +1,29 @@
+import { useSelector } from 'react-redux';
+import Mission from './Mission';
+
 function Missions() {
+  const { missions } = useSelector((state) => state.missions);
+
   return (
     <section>
-      Rockets
+      <table>
+        <thead>
+          <tr>
+            <th scope="col" style={{ width: '10%' }}>Mission</th>
+            <th scope="col" style={{ width: '60%' }}>Description </th>
+            <th scope="col" style={{ width: '15%' }}>Status</th>
+            <th scope="col" style={{ width: '15%' }}>Join</th>
+          </tr>
+        </thead>
+        { missions.map((mission) => (
+          <tbody key={mission.id}>
+            <Mission
+              name={mission.name}
+              description={mission.description}
+            />
+          </tbody>
+        ))}
+      </table>
     </section>
   );
 }
